fe683b9cce409a84fddd04cb9948813c972c1919
Goran Lukic <goranluki@gmail.com>
2011-09-14 17:05:00 +0300

    OCCMobileExample2 - OCCMobileExample2.java

--- OCCMobileExample2/src/occ/mobile/example2/OCCMobileExample2.java
+++ OCCMobileExample2/src/occ/mobile/example2/OCCMobileExample2.java
@@ -11,12 +11,11 @@ import android.view.View.OnClickListener;
 /**
  * 	@author Goran Lukic
  * 	@since  04.09.2011
- *  @see    http://github.com/goranl
  */
 
 public class OCCMobileExample2 extends Activity {
 	
-	OCC occNative;
+    OCC occNative;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,22 +32,22 @@ public class OCCMobileExample2 extends Activity {
         EditText value2 = (EditText) findViewById(R.id.value2);
         
         public void onClick(View v) {
-        	double v1 =0;
-			double v2 =0;
-			double res=0;
-        	if (value1.length() <=0 ) {
-        		value1.setError(getText(R.string.error));  
-        	}
-        	else if (value2.length() <=0 ) {
-        		value2.setError(getText(R.string.error));  
-        	}
-        	else if ((value1.length() > 0) && (value2.length()) > 0) 
-        	{
-        		v1 = Double.parseDouble(value1.getText().toString());
-        		v2 = Double.parseDouble(value2.getText().toString());
-        		res = occNative.result(v1, v2);
-        		result.setText(new Double(res).toString());
-        	}
+        	 double v1 =0;
+			     double v2 =0;
+			     double res=0;
+        	 if (value1.length() <=0 ) {
+        		  value1.setError(getText(R.string.error));  
+        	 }
+        	 else if (value2.length() <=0 ) {
+        		  value2.setError(getText(R.string.error));  
+        	 }
+        	 else if ((value1.length() > 0) && (value2.length()) > 0) 
+        	 {
+        		  v1 = Double.parseDouble(value1.getText().toString());
+        	 	  v2 = Double.parseDouble(value2.getText().toString());
+        		  res = occNative.result(v1, v2);
+        		  result.setText(new Double(res).toString());
+        	 }
          }  
       }); 
    }
