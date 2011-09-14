c20461f5475dc3f0a3cd4d95c6033975300dda36
Goran Lukic <goranluki@gmail.com>
2011-09-14 16:58:11 +0300

    Edited OCCMobileExample2/src/occ/mobile/example2/OCC.java via GitHub

--- OCCMobileExample2/src/occ/mobile/example2/OCC.java
+++ OCCMobileExample2/src/occ/mobile/example2/OCC.java
@@ -12,19 +12,20 @@ public class OCC {
   static 
   {
 	 try {
-	  	  System.loadLibrary("TKernel");  
-	  	  System.loadLibrary("TKMath"); 
-	  	  System.loadLibrary("TKG2d");
-	  	  System.loadLibrary("TKG3d");
-	  	  System.loadLibrary("TKGeomBase");    
-		  System.loadLibrary("TKBRep");
-		  System.loadLibrary("TKGeomAlgo");
-		  System.loadLibrary("TKTopAlgo");    
-		  System.loadLibrary("TKPrim");   
-	  	  System.loadLibrary("occ_native");
+        System.loadLibrary("TKernel");
+        System.loadLibrary("TKMath");
+        System.loadLibrary("TKG2d");
+        System.loadLibrary("TKG3d");
+        System.loadLibrary("TKGeomBase");
+        System.loadLibrary("TKBRep");
+        System.loadLibrary("TKGeomAlgo");
+        System.loadLibrary("TKTopAlgo");
+        System.loadLibrary("TKPrim");
+        System.loadLibrary("occ_native");
 	  	  
 	  } catch (UnsatisfiedLinkError use) {
-		    Log.e("OCC", "WARNING:WARNING:WARNING: Could not load librarys");
+        
+        Log.e("OCC", "WARNING:WARNING:WARNING: Could not load librarys");
 		}
   }
 
